cd6572dff0a03737e0b304e1df90df59d2e8b0c4
Tom Fasquelle <tom.fasquelle@etu.univ-lyon1.fr>
2026-03-27 15:25:16 +0100

    sort working, need to improve output

--- 4eTP/tiersort.py
+++ 4eTP/tiersort.py
@@ -1,7 +1,17 @@
 def tiersort(tab, start, end, step):
     length = len(tab)
-    if (start == 0 and end == len(tab)):
-        tiersort(tab,0,int(2*length/3), step+1)
+    if (start == 0 and end == length):
+        step+=1
+        tiersort(tab,0,int(2*length/3), step)
+    else:
+        if step == 3:
+            return
+        else :
+            step+=1
+            sort(tab, start, end)
+            tiersort(tab,int(length/3),length, step)
+
+def sort(tab , start, end):
     subtab = tab[start:end]
     if(start > 0):
         other = tab[0:start]
@@ -11,13 +21,8 @@ def tiersort(tab, start, end, step):
         other = tab[end:len(tab)]
         subtab.sort()
         tab = subtab + other
-    if(start == 0 and end != length ):
-        if step == 3:
-            print(tab)
-            return tab
-        else :
-            tiersort(tab,int(length/3),length, step+1)
+    print("Tab ", tab)
 
-tab= [2,5,6,12,9]
+tab= [2,15,6,12,9]
 print("Debut", tab)
-tiersort(tab,0,len(tab), 0)
+print(tiersort(tab,0,len(tab), 0))
